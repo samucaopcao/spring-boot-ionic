986d855eff5f07805c436226e2f45c145560c427
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-03-11 22:10:26 -0300

    Curso Spring Boot - Nélio Alves: Inserção de Pessoas na Base de Dados

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,18 +1,31 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1,"Carlos da Silva","carlos@gmail.com");
-		Pessoa p2 = new Pessoa(1,"Carlos da Silva","carlos@gmail.com");
-		Pessoa p3 = new Pessoa(1,"Carlos da Silva","carlos@gmail.com");
+		Pessoa p1 = new Pessoa(null,"Carlos da Silva","carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null,"Carlos da Silva","carlos@gmail.com");
+		Pessoa p3 = new Pessoa(null,"Carlos da Silva","carlos@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();//Como não é uma simples leitura do BD é necessário uma transação com o banco.
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();//Esse trecho é necessário para confirmar a gravação no Banco.
+		
+		System.out.println("Tudo pronto!");
+
 	}
 
 }
